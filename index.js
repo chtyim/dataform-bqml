4b6975f9255a4f0f2676c6bf5922557b9dee9de9
Terence Yim <terencey@google.com>
2024-09-06 10:26:40 -0700

    WIP
    WIP
    
    fixup
    
    fixup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
+const config = require("./modules/config");
 const structured_table_ml = require("./modules/structured_table_ml");
 const object_table_ml = require("./modules/object_table_ml");
 
-module.exports = {...structured_table_ml, ...object_table_ml};
+module.exports = {...config, ...structured_table_ml, ...object_table_ml};
---
+++ modules/config.js
@@ -0,0 +1,7 @@
+const config = {
+  auto_declare: false,
+}
+
+module.exports = {
+  config: config,
+}
